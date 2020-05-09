85f983740f13eb0ba30eda33ca440d70c16a168e
Danilo Chang <ray2501@gmail.com>
2020-05-09 11:09:16 +0800

    Bump to version 0.4

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded thiredis 0.3 \
-    [list load [file join $dir libthiredis0.3.so] thiredis]
+package ifneeded thiredis 0.4 \
+    [list load [file join $dir libthiredis0.4.so] thiredis]
