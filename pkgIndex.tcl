041dc33d38f3ca530fba5ff75c1f0a30629a4385
Danilo Chang <ray2501@gmail.com>
2023-12-15 20:16:40 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded thiredis 0.6 \
-    [list load [file join $dir libthiredis0.6.so] thiredis]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded thiredis 0.6 \
+	    [list load [file join $dir libtcl9thiredis0.6.so] [string totitle thiredis]]
+} else {
+    package ifneeded thiredis 0.6 \
+	    [list load [file join $dir libthiredis0.6.so] [string totitle thiredis]]
+}
