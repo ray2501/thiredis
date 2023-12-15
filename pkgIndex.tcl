57efa6b3a97096b4b330bca607235ffb5702417a
Danilo Chang <ray2501@gmail.com>
2023-12-15 20:20:43 +0800

    Bump to version 0.7

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded thiredis 0.6 \
-	    [list load [file join $dir libtcl9thiredis0.6.so] [string totitle thiredis]]
+    package ifneeded thiredis 0.7 \
+	    [list load [file join $dir libtcl9thiredis0.7.so] [string totitle thiredis]]
 } else {
-    package ifneeded thiredis 0.6 \
-	    [list load [file join $dir libthiredis0.6.so] [string totitle thiredis]]
+    package ifneeded thiredis 0.7 \
+	    [list load [file join $dir libthiredis0.7.so] [string totitle thiredis]]
 }
